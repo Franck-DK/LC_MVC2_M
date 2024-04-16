e1412486b8ce424695cc45e9ecf28ac6aaf7657c
Franck-DK <franck.dang.kum-1@ou.edu>
2024-04-15 22:54:47 -0500

    Add project files.

---
+++ LC_MVC2_M/Models/ErrorViewModel.cs
@@ -0,0 +1,12 @@
+namespace LC_MVC2_M.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId
+        {
+            get; set;
+        }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
